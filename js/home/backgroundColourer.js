518bff2ef25fc4fb074b7ba42b8d494df3625d5b
braxtonhall <braxtonjhall@gmail.com>
2022-05-04 16:41:37 -0700

    #29 Random background colours have smooth transition

--- js/home/backgroundColourer.js
+++ js/home/backgroundColourer.js
@@ -1,19 +1,27 @@
-const BG_RANDOM_TIME_MIN = 1000;
-const BG_RANDOM_TIME_MAX = 2000;
+const BG_RANDOM_MILLISECONDS_MIN = 1000;
+const BG_RANDOM_MILLISECONDS_MAX = 4000;
 
 const getRandomColour = () => {
     return '#' + Math.floor(Math.random()*16777215).toString(16);
 };
 
 const setBackgroundColour = (transition) => {
-    $("body").css({
+    const $bg2 = $("#bg-2");
+    $("#bg-1").css({
+        'background-image': $bg2.css('background-image'),
+    });
+    $bg2.finish();
+    $bg2.css({
         'background-image': `linear-gradient(${getRandomColour()}, ${getRandomColour()})`,
-        'transition': `${transition}ms ease-in-out`,
-    })
+        opacity: 0,
+    });
+    $bg2.animate({
+        opacity: 1,
+    }, transition);
 };
 
 const loopBackgroundColourChange = () => {
-    const time = random(BG_RANDOM_TIME_MIN, BG_RANDOM_TIME_MAX);
+    const time = random(BG_RANDOM_MILLISECONDS_MIN, BG_RANDOM_MILLISECONDS_MAX);
     setBackgroundColour(time);
     setTimeout(loopBackgroundColourChange, time);
 };
