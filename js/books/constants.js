07f50515d9916dd7c9d7fdc2972b4e1dd28f8b10
braxtonhall <braxtonjhall@gmail.com>
2022-05-14 00:39:26 -0700

    Add a pdf viewer to Books page

---
+++ js/books/constants.js
@@ -0,0 +1,23 @@
+const BOOKS = [
+    {
+        id: "test-1",
+        name: "Testing Name",
+        preview: "../img/instagram.png",
+        description: "This page is currently under construction but when it is finished it will look amazzing with all my books!!!!!! WOAHOAOAOAAHHOAAAAAAHO!!!!!",
+        pdf: "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
+    },
+    {
+        id: "test-2",
+        name: "Testing Name",
+        preview: "../img/instagram.png",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac efficitur lorem, vel tempor lacus. Integer id aliquam lectus, quis tempor sem. Sed eu laoreet risus, ac maximus velit. Etiam dui nisi, auctor lobortis sem aliquam, auctor egestas tellus. Sed non lacus vitae eros luctus varius. Ut id placerat erat. Quisque eget libero ligula. Pellentesque nec nibh felis.",
+        pdf: "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
+    },
+    {
+        id: "test-3",
+        name: "Testing Name",
+        preview: "../img/instagram.png",
+        description: "This page is currently under construction but when it is finished it will look amazzing with all my books!!!!!! WOAHOAOAOAAHHOAAAAAAHO!!!!!",
+        pdf: "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf",
+    }
+];
---
+++ js/books/pdfLoader.js
@@ -0,0 +1,88 @@
+// Loaded via <script> tag, create shortcut to access PDF.js exports.
+var pdfjsLib = window['pdfjs-dist/build/pdf'];
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdn.jsdelivr.net/npm/pdfjs-dist@2.13.216/build/pdf.worker.js';
+
+class PDF {
+    static SCALE = 0.8;
+
+    pdfDoc = null;
+    pageNum = 1;
+    pageRendering = false;
+    pageNumPending = null;
+    context;
+
+    constructor(id, url) {
+        const $modal = $(`#${id}`);
+        const $canvas = $modal.find('canvas');
+        this.context = $canvas[0].getContext('2d');
+        $modal.find(".prev").on('click', this.onPrevPage($modal));
+        $modal.find(".next").on('click', this.onNextPage($modal));
+
+        pdfjsLib.getDocument(url).promise.then((pdfDoc) => {
+            this.pdfDoc = pdfDoc;
+            $modal.find(".page-count").text(pdfDoc.numPages);
+            // Initial/first page rendering
+            this.renderPage(this.pageNum, $modal);
+        });
+    }
+
+    queueRenderPage(num, $modal) {
+        if (this.pageRendering) {
+            this.pageNumPending = num;
+        } else {
+            this.renderPage(num, $modal);
+        }
+    }
+
+    onPrevPage = ($modal) => () => {
+        if (this.pageNum <= 1) {
+            return;
+        }
+        this.pageNum--;
+        this.queueRenderPage(this.pageNum, $modal);
+    };
+
+    onNextPage = ($modal) => () => {
+        if (this.pageNum >= this.pdfDoc.numPages) {
+            return;
+        }
+        this.pageNum++;
+        this.queueRenderPage(this.pageNum, $modal);
+    };
+
+    renderPage(num, $modal) {
+        this.pageRendering = true;
+        // Using promise to fetch the page
+        this.pdfDoc.getPage(num).then((page) => {
+            const viewport = page.getViewport({scale: PDF.SCALE});
+            this.context.canvas.height = viewport.height;
+            this.context.canvas.width = viewport.width;
+
+            // Render PDF page into canvas context
+            const renderContext = {
+                canvasContext: this.context,
+                viewport
+            };
+
+            page.render(renderContext).promise.then(() => {
+                this.pageRendering = false;
+                if (this.pageNumPending !== null) {
+                    // New page rendering is pending
+                    this.renderPage(this.pageNumPending, $modal);
+                    this.pageNumPending = null;
+                }
+            });
+        });
+
+        // Update page counters
+        $modal.find('.page-num').text(num);
+    }
+}
+
+
+function init() {
+    BOOKS.forEach((book) => new PDF(book.id, book.pdf));
+}
+
+$(init);
