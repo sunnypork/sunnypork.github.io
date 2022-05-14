15e6cf8d9a29653cbfc7fe5b36edf7d0919fdac1
braxtonhall <braxtonjhall@gmail.com>
2022-05-14 14:38:49 -0700

    #35 #37 Breathing and wait for completed animations

--- js/home/buttonEventHandlers.js
+++ js/home/buttonEventHandlers.js
@@ -1,12 +1,23 @@
+const canAnimate = ($element) =>
+    !$element.is(":hover") && !$element.is(':animated');
+
+const ifCanAnimate = (fn) => (id, ...args) => {
+    const $element = $(id);
+    if (canAnimate($element)) {
+        fn($element, ...args);
+    }
+};
+
 const handleHeader = () => {
     // Get a random rotation
     const degrees = random(-HEADING_ROTATION_ABS_MAX_DEGREES, HEADING_ROTATION_ABS_MAX_DEGREES);
     editCssWithQuery("#heading", {
         'transform' : `rotate(${degrees}deg)`,
+        myRotationProperty: degrees
     });
 };
 
-const handleVideoPortfolio = () => {
+const handleBooks = () => {
     // Moves along the x axis
     const leftPosition = random(0, 96);
     editCssWithQuery("#port-container", {
@@ -15,7 +26,7 @@ const handleVideoPortfolio = () => {
 };
 
 const handleVideoArt = () => {
-    drip("#art");
+    breathe("#art");
 };
 
 const handleStills = () => {
@@ -30,14 +41,12 @@ const handleAboutMe = () => {
     wiggle("#me-container");
 };
 
-const wiggle = (id) => {
+const wiggle = ifCanAnimate(($element) => {
     const maxDistance = WIGGLE_DISTANCE;
     const positions = random(WIGGLE_MIN_POSITIONS, WIGGLE_MAX_POSITIONS);
     const duration = randomAnimationDuration();
     const frameDuration = Math.round(duration / positions + 1);
 
-    const $element = $(id);
-    $element.stop(true);
     // Must match .button-container and .right-button-container in home.css
     const top = "80px"; // $element.css("top");
     const left = "40px"; // $element.css("left");
@@ -54,11 +63,9 @@ const wiggle = (id) => {
         $element.animate(animation, frameDuration);
     }
     $element.animate({top, left}, frameDuration);
-};
+});
 
-const drip = (id) => {
-    const $element = $(id);
-    $element.stop(true);
+const drip = ifCanAnimate(($element) => {
     // should match height of .menu-button in home.css
     const height = "144px"; // $element.css("height");
     const heightPx = parseInt(height);
@@ -66,29 +73,51 @@ const drip = (id) => {
 
     const duration = randomAnimationDuration();
     const dripSpeed = DRIP_ANIMATION_RATIO * duration;
-    const snapBackSpeed = (DRIP_ANIMATION_RATIO - 1) * duration;
+    const snapBackSpeed = (1 - DRIP_ANIMATION_RATIO) * duration;
     $element.animate({
         height: `${newHeight}px`,
     }, dripSpeed);
     $element.animate({
         height,
     }, snapBackSpeed);
-};
+});
 
-const editCssWithQuery = (query, css) => {
-    const $element = $(query);
-    if (!$element.is(":hover")) {
-        const transition = randomAnimationDuration();
-        $element.css({
-            ...css,
-            'transition': `${transition}ms ease-in-out`,
-        });
+const breathe = ifCanAnimate(($element) => {
+    const padding = `${BREATHE_ANIMATION_PADDING}px`;
+    const negativePadding = `-${padding}`;
+
+    const totalDuration = randomAnimationDuration();
+    const duration = totalDuration / BREATHE_COUNT;
+    const breatheIn = BREATHE_ANIMATION_RATIO * duration;
+    const breatheOut = (1 - BREATHE_ANIMATION_RATIO) * duration;
+
+    for (let i = 0; i < BREATHE_COUNT; i = i + 1) {
+        $element
+            .animate({padding, top: negativePadding, left: negativePadding}, breatheIn)
+            .animate({padding: 0, top: 0, left: 0}, breatheOut);
     }
-};
+});
+
+const editCssWithQuery = ifCanAnimate(($element, css) => {
+    const transition = randomAnimationDuration();
+    $element.animate({
+        ...css,
+    }, {
+        step: function(now, fx) {
+            if (fx.prop === "myRotationProperty") {
+                $(this).css('-webkit-transform','rotate('+now+'deg)');
+                $(this).css('-moz-transform','rotate('+now+'deg)');
+                // add Opera, MS etc. variants
+                $(this).css('transform','rotate('+now+'deg)');
+            }
+        },
+        duration: transition,
+    }, 'ease-in-out');
+});
 
 const handlers = [
     handleHeader,
-    handleVideoPortfolio,
+    handleBooks,
     handleVideoArt,
     handleStills,
     handleAboutMe,
