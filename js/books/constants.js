f6a1d545811b1d62a6be6b81c63d3470643f5a62
sunnypork <85819748+sunnypork@users.noreply.github.com>
2022-05-19 23:29:22 -0700

    Update constants.js

--- js/books/constants.js
+++ js/books/constants.js
@@ -17,14 +17,14 @@ const BOOKS = [
         id: "weird-zoo",
         name: "Weird Zoo",
         preview: "../img/books/weird-zoo.jpeg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac efficitur lorem, vel tempor lacus. Integer id aliquam lectus, quis tempor sem. Sed eu laoreet risus, ac maximus velit. Etiam dui nisi, auctor lobortis sem aliquam, auctor egestas tellus. Sed non lacus vitae eros luctus varius. Ut id placerat erat. Quisque eget libero ligula. Pellentesque nec nibh felis.",
+        description: "I made this book by etching a big, long copper plate and printing it with intalio ink. If you want to buy one from me, I will print a fresh one out for ya.",
         pdf: "../weirdzoo.pdf",
     },
     {
         id: "growth",
         name: "Growth",
         preview: "../img/books/growth/korean-melon.jpg",
-        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac efficitur lorem, vel tempor lacus. Integer id aliquam lectus, quis tempor sem. Sed eu laoreet risus, ac maximus velit. Etiam dui nisi, auctor lobortis sem aliquam, auctor egestas tellus. Sed non lacus vitae eros luctus varius. Ut id placerat erat. Quisque eget libero ligula. Pellentesque nec nibh felis.",
+        description: "Made this book when I was 16. My thoughts and happy thought and sad thoughts and Paul and Jaguar.",
         pdf: "../growthzine.pdf",
     },
 ];
