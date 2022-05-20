90d3b6cdc8a3c9fe2b9f11a10f1b445196935519
sunnypork <85819748+sunnypork@users.noreply.github.com>
2022-05-19 23:27:05 -0700

    Update constants.js

--- js/books/constants.js
+++ js/books/constants.js
@@ -18,13 +18,13 @@ const BOOKS = [
         name: "Weird Zoo",
         preview: "../img/books/weird-zoo.jpeg",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac efficitur lorem, vel tempor lacus. Integer id aliquam lectus, quis tempor sem. Sed eu laoreet risus, ac maximus velit. Etiam dui nisi, auctor lobortis sem aliquam, auctor egestas tellus. Sed non lacus vitae eros luctus varius. Ut id placerat erat. Quisque eget libero ligula. Pellentesque nec nibh felis.",
-        pdf: "../pdf/weirdzoo.pdf",
+        pdf: "../weirdzoo.pdf",
     },
     {
         id: "growth",
         name: "Growth",
         preview: "../img/books/growth/korean-melon.jpg",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ac efficitur lorem, vel tempor lacus. Integer id aliquam lectus, quis tempor sem. Sed eu laoreet risus, ac maximus velit. Etiam dui nisi, auctor lobortis sem aliquam, auctor egestas tellus. Sed non lacus vitae eros luctus varius. Ut id placerat erat. Quisque eget libero ligula. Pellentesque nec nibh felis.",
-        pdf: "../pdf/growthzine.pdf",
+        pdf: "../growthzine.pdf",
     },
 ];
