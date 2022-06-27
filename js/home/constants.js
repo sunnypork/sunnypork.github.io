d68d726ba94ac5a5ba147c0940e58ac3a7c3867c
braxtonhall <braxtonjhall@gmail.com>
2022-06-27 01:14:11 -0700

    Increase chance of playing the game

--- js/home/constants.js
+++ js/home/constants.js
@@ -50,4 +50,4 @@ const WIGGLE_ABS_MAX_DEGREES = 5;
 // Minimum width of the screen to enable the game
 const MIN_WIDTH_TO_PLAY = 768;
 // Chance to play the game is 1 out of this number
-const CHANCE_TO_PLAY_ONE_OF = 50;
+const CHANCE_TO_PLAY_ONE_OF = 20;
