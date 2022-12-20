4f755b71ce4c581846c585a520034322b7ad7d2e
braxtonhall <braxtonjhall@gmail.com>
2022-12-20 18:25:57 +0100

    Add a little game to the about me page

--- js/me.js
+++ js/me.js
@@ -4,6 +4,10 @@ const FLIP_INTERVAL_MILLISECONDS = 1500;
 let flipped = false;
 const flipClass = 'flip';
 
+const maxHappiness = 5;
+const happinessDecay = 800;
+let happiness = 0;
+
 const loopFlip = () => {
     const $flipper = $('#flipper');
     if (!$flipper.is(":hover")) {
@@ -16,7 +20,27 @@ const loopFlip = () => {
     }
 };
 
+const incHappiness = () => {
+    if (happiness < maxHappiness) {
+        happiness = happiness + 1;
+        if (happiness === maxHappiness) {
+            alert("Woof!");
+        }
+    }
+};
+
+const decHappiness = () => {
+    if (happiness > 0) {
+        happiness = happiness - 1;
+        console.log(happiness);
+    }
+};
+
 const init = () => {
+    $(".scratch-box").on("click", () => {
+        incHappiness();
+        setTimeout(decHappiness, happinessDecay);
+    });
     setInterval(loopFlip, FLIP_INTERVAL_MILLISECONDS);
 };
 
